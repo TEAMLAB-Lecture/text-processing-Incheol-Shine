006b089e1523692cd69966123b3c64276b14fc7a
Incheol-Shine <parpedian@gmail.com>
2021-01-19 22:31:38 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -67,7 +67,8 @@ def no_vowels(input_string):
     """
     queue = ''
     for i in input_string:
-        if i != 'a' or i != 'e' or i != 'i' or i != 'o' or i != 'u':
+        if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u' and\
+            i != 'A' and i != 'E' and i != 'I' and i != 'O' and i != 'U':
             queue += i
 
     no_vowel_string = str(queue)
